4671d3afad72ffa48959c9a6fcaac027d1d1f1c9
samuelbd8 <samuel.beltran@uao.edu.co>
2026-04-12 10:51:20 -0500

    Se crea la clase Estudiante que hereda de Persona con atributos académicos

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -8,6 +8,33 @@ package modelo;
  *
  * @author samue
  */
-public class Estudiante {
+public class Estudiante extends Persona {
+
+    private String codigo;
+    private String grado;
+    private double promedio;
+
+    public Estudiante(String nombre, String direccion, String telefono, String fechaNacimiento,
+                      String codigo, String grado, double promedio) {
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.codigo = codigo;
+        this.grado = grado;
+        this.promedio = promedio;
+    }
+
+   
+    public String getCodigo() { return codigo; }
+    public String getGrado() { return grado; }
+    public double getPromedio() { return promedio; }
+
+   
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public void setGrado(String grado) { this.grado = grado; }
+    public void setPromedio(double promedio) { this.promedio = promedio; }
+
     
+    @Override
+    public String toString() {
+        return getNombre() + " - Codigo: " + codigo + " - Grado: " + grado + " - Promedio: " + promedio;
+    }
 }
