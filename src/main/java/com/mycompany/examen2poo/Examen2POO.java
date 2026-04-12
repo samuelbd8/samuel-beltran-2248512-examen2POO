7510d38212ea33c3e12c0dd7df4f7d2dcf968099
samuelbd8 <samuel.beltran@uao.edu.co>
2026-04-12 10:38:17 -0500

    Se crea la estructura general que llevará el proyecto. Se crean los paquetes modelo, vista y controlador con sus respectivas clases.

---
+++ src/main/java/com/mycompany/examen2poo/Examen2POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen2poo;
+
+/**
+ *
+ * @author samue
+ */
+public class Examen2POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
