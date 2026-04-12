23893cf97dbd5878cdfc8fec25797337a900ec9b
samuelbd8 <samuel.beltran@uao.edu.co>
2026-04-12 10:41:59 -0500

    Implementar clase Persona con atributos básicos, constructores y métodos getter/setter

--- src/main/java/modelo/Persona.java
+++ src/main/java/modelo/Persona.java
@@ -9,5 +9,28 @@ package modelo;
  * @author samue
  */
 public class Persona {
-    
+
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String fechaNacimiento;
+
+    public Persona(String nombre, String direccion, String telefono, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+   
+    public String getNombre() { return nombre; }
+    public String getDireccion() { return direccion; }
+    public String getTelefono() { return telefono; }
+    public String getFechaNacimiento() { return fechaNacimiento; }
+
+  
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setFechaNacimiento(String fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
 }
